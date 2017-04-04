6131e313f17ed3d0972895bc6b7928af578315a1
camlien <kieu.camlien@hotmail.fr>
2017-04-04 20:41:15 +0000

    .

--- client/app/app.js
+++ client/app/app.js
@@ -1,13 +1,11 @@
 var app = angular.module("app", ['ngAnimate', 'leaflet-directive','ui.bootstrap', 'ngRoute','checklist-model']);
-<<<<<<< HEAD
+
 
 
 
 app.constant('API_URL', 'http://localhost/citydata/api/rest/');
 
-=======
-    app.constant('API_URL', 'http://project2.local/citydata/api/rest/');
->>>>>>> 2aa88285f40157471d2f7317e845a3f497a603bd
+
 
 app.config(function($logProvider){
 $logProvider.debugEnabled(false);
